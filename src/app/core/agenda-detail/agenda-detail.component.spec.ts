4dc6b27d3c2f4752f38e72a1bf9fda3737ceb51a
Julien Delvaux <delvaux.ju@gmail.com>
2020-11-29 14:52:33 +0100

    Upgrade to Angular 11

--- src/app/core/agenda-detail/agenda-detail.component.spec.ts
+++ src/app/core/agenda-detail/agenda-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AgendaDetailComponent } from './agenda-detail.component';
 
@@ -6,7 +6,7 @@ describe('AgendaDetailComponent', () => {
   let component: AgendaDetailComponent;
   let fixture: ComponentFixture<AgendaDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AgendaDetailComponent ]
     })
